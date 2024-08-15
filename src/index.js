7da5f9f0d0eac578ab5ea72b5721a0cec2904861
ahmedalsanadi <ahmed11alsanadi11@gmail.com>
2024-08-16 02:36:42 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const newTaskDescription = document.getElementById('new-task-description').value;
+    const newTask = document.createElement('li');
+    newTask.textContent = newTaskDescription;
+    taskList.appendChild(newTask);
+    form.reset();
+  });
 });
